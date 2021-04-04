df3fe88ef81f324abcb1eea3d9f2be0204f228bf
Cade Ellsworth <caedmd2100@gmail.com>
2021-04-03 20:31:41 -0700

    got something to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    var finalPassword = generatePassword();
     var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+    passwordText.value = finalPassword;
 
 }
 
@@ -18,24 +18,28 @@ function generatePassword() {
     while (!lowerCaseConfirm || !upperCaseConfirm || !numericConfirm || !specialConfirm) {
 
         lowerCaseConfirm = confirm("Would you like lowercase characters in your password?");
-        
+
         upperCaseConfirm = confirm("Would you like uppercase characters in your password?");
-        
+
         numericConfirm = confirm("Would you like numeric characters in your password?");
-        
-        specialConfirm = confirm("Would you like special characters in your password?");      
+
+        specialConfirm = confirm("Would you like special characters in your password?");
     }
+    
     var howMany = 0
     while (howMany <= 7 || howMany >= 129 || isNaN(howMany)) {
         howMany = prompt("How many characters would you like your password to be. Select a number between 8-128.");
     }
+    
     var lowerCase = ["a", "b", "c", "d"]
-    var blank = []
-    var passCode = ""
+    var characterBank = []
+    // var passCode = ""
     if (lowerCaseConfirm) {
-        blank = blank.concat(lowerCase);
-        passCode += blankFunction(lowerCase);
+        characterBank = characterBank.concat(lowerCase);
+        // passCode += characterBank(lowerCase);
     }
+    var finalPassword = characterBank[Math.floor(Math.random() * characterBank.length)];
+    return finalPassword
 }
 
 // Add event listener to generate button
